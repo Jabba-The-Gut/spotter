8f165b8ea0fca261dde8cce79f52fbdf61001ca2
David Gut <gud@iflow.ch>
2020-09-08 10:42:26 +0200

    Removed exposed spotify keys

--- src/main/python/spotter/test.py
+++ src/main/python/spotter/test.py
@@ -1,8 +1,8 @@
 import spotipy.util as util
 
-username = 'gavee_123'
-client_id ='5c87be52b8f94391993c896f21798ef4'
-client_secret = '453f165af7104da1a51e730608c5fd03'
+username = ''
+client_id =''
+client_secret = ''
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-read-recently-played'
 
